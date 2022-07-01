f855a94cadb2062c46ad80567a91357276080785
rotem711 <rotemskandrani@gmail.com>
2022-07-01 09:16:02 +0200

    added tailwind config and new blocks

--- utils/rendering/inventory.ts
+++ utils/rendering/inventory.ts
@@ -2,17 +2,12 @@ import React, { FunctionComponent } from 'react'
 import dynamic from 'next/dynamic'
 import { typename as TextImageTypename } from '../../components/blocks/text_image/text_image'
 import TextImageQuery from '../../components/blocks/text_image/text_image.graphql'
-
 import { typename as HeroBlockTypename } from '../../components/blocks/hero/hero'
 import HeroBlockQuery from '../../components/blocks/hero/hero.graphql'
-
 import { typename as QuoteSliderBlockTypename } from '../../components/blocks/quote_slider/quote_slider'
 import QuoteSliderBlockQuery from '../../components/blocks/quote_slider/quote_slider.graphql'
-
-
 import { typename as LogoGridBlockTypename } from '../../components/blocks/logo_grid/logo_grid'
 import LogoGridBlockQuery from '../../components/blocks/logo_grid/logo_grid.graphql'
-
 
 // ##HYGEN_IMPORT##
 // do not modify the above line since its used by hygen to generate the block list
