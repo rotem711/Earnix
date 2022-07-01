5e6d1e588e16ed8ab9283d5b302e7d4fefa98132
rotem711 <rotemskandrani@gmail.com>
2022-07-01 09:15:45 +0200

    added tailwind config and new blocks

---
+++ components/blocks/logo_grid/logo_grid.graphql.ts
@@ -0,0 +1,5 @@
+export default `
+fragment BlockLogoGrid on Set_Replicator_BlockLogoGrid {
+    type
+}
+`
--- utils/rendering/inventory.ts
+++ utils/rendering/inventory.ts
@@ -6,6 +6,14 @@ import TextImageQuery from '../../components/blocks/text_image/text_image.graphq
 import { typename as HeroBlockTypename } from '../../components/blocks/hero/hero'
 import HeroBlockQuery from '../../components/blocks/hero/hero.graphql'
 
+import { typename as QuoteSliderBlockTypename } from '../../components/blocks/quote_slider/quote_slider'
+import QuoteSliderBlockQuery from '../../components/blocks/quote_slider/quote_slider.graphql'
+
+
+import { typename as LogoGridBlockTypename } from '../../components/blocks/logo_grid/logo_grid'
+import LogoGridBlockQuery from '../../components/blocks/logo_grid/logo_grid.graphql'
+
+
 // ##HYGEN_IMPORT##
 // do not modify the above line since its used by hygen to generate the block list
 
@@ -27,6 +35,20 @@ const blocks:Block[] = [
     component: dynamic(() => import('../../components/blocks/hero/hero')),
     query: HeroBlockQuery,
     typename: HeroBlockTypename,
+    extraQueries: undefined,
+  },
+
+  {
+    component: dynamic(() => import('../../components/blocks/quote_slider/quote_slider')),
+    query: QuoteSliderBlockQuery,
+    typename: QuoteSliderBlockTypename,
+    extraQueries: undefined,
+  },
+
+  {
+    component: dynamic(() => import('../../components/blocks/logo_grid/logo_grid')),
+    query: LogoGridBlockQuery,
+    typename: LogoGridBlockTypename,
     extraQueries: undefined,
   },
 
