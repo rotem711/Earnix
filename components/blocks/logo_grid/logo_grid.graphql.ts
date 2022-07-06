7c235dc0f34ef372db4947b14742e4d1a395436f
rotem711 <rotemskandrani@gmail.com>
2022-07-06 16:00:04 +0200

    Create component logo grid

--- components/blocks/logo_grid/logo_grid.graphql.ts
+++ components/blocks/logo_grid/logo_grid.graphql.ts
@@ -1,5 +1,12 @@
 export default `
 fragment BlockLogoGrid on Set_Replicator_BlockLogoGrid {
-    type
+    type,
+    lg_cta_url
+    lg_cta_title
+    lg_logos {
+        logo {
+            ...CMSAsset
+        }
+    }
 }
 `
