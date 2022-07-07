ed7d4b3f798e2ef365fb4206eba0b2c25deb95f4
rotem711 <rotemskandrani@gmail.com>
2022-07-07 14:26:45 -0500

    adding IconsWText compomnent

--- components/blocks/icons_w_text/icons_w_text.interface.ts
+++ components/blocks/icons_w_text/icons_w_text.interface.ts
@@ -1,3 +1,9 @@
 export default interface IconsWTextInterface {
-    type: string
+  type: string
+  ict_copy: string
+  ict_cta_title: string
+  ict_cta_url: string
+  ict_headline: string
+  ict_icons: any
+  ict_background:any
 }
--- components/blocks/icons_w_text/icons_w_text.tsx
+++ components/blocks/icons_w_text/icons_w_text.tsx
@@ -1,15 +1,48 @@
+/* eslint-disable react/no-array-index-key */
+/* eslint-disable react/jsx-one-expression-per-line */
 import React, { FunctionComponent } from 'react'
+import Image from 'next/image'
+import Button from 'components/generic/button/button'
 import styles from './icons_w_text.module.scss'
 import IconsWTextInterface from './icons_w_text.interface'
 
 export const typename = 'Set_Replicator_BlockIconsWText'
 
-const IconsWTextBlock:FunctionComponent<{ block: IconsWTextInterface }> = ({ block }) => (
-  <div
-    className={`${styles.root} container`}
-  >
-    Iconswtext Block:
-    {block.type}
+const IconsWTextBlock: FunctionComponent<{ block: IconsWTextInterface }> = ({
+  block,
+}) => (
+  <div className={`${styles.root} container`}>
+    <div className={styles.contentWrapper}>
+      <div className={styles.headline}>{block.ict_headline}</div>
+      <div
+        className={styles.copy}
+        dangerouslySetInnerHTML={{ __html: block.ict_copy }}
+      />
+      <div className={styles.iconGrid}>
+        {block.ict_icons.map((icon: any, key: number) => (
+          <div className={styles.iconWrapper} key={key}>
+            <div className={styles.iconImage}>
+              <Image
+                src={`https://${process.env.NEXT_PUBLIC_ASSET_DOMAIN}${icon.icon.url}`}
+                width={130}
+                height={130}
+              />
+            </div>
+            <div className={styles.iconText}> {icon.text}</div>
+          </div>
+        ))}
+      </div>
+      <div className={styles.ctaWrapper}>
+        <Button href={block.ict_cta_url} title={block.ict_cta_title} />
+      </div>
+      <div className={styles.background}>
+        <Image
+          src={`https://${process.env.NEXT_PUBLIC_ASSET_DOMAIN}${block.ict_background.url}`}
+          width={635}
+          height={274}
+        />
+      </div>
+    </div>
   </div>
 )
 
