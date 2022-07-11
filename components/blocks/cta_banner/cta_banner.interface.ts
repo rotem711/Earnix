fe2ea914b4fdd7ad30714b1a3070ae6139dca880
rotem711 <rotemskandrani@gmail.com>
2022-07-11 09:17:37 -0500

    fix: adding the Assets interface

--- components/blocks/cta_banner/cta_banner.interface.ts
+++ components/blocks/cta_banner/cta_banner.interface.ts
@@ -1,8 +1,10 @@
+import Asset from 'interfaces/asset'
+
 export default interface CtaBannerInterface {
   type: string
   ctab_copy: string
   ctab_cta_title: string
   ctab_cta_url: string
   ctab_headline: string
-  ctab_ctaBackground: any
+  ctab_ctaBackground: Asset
 }
