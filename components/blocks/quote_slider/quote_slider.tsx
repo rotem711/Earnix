4e16373438a332fb2af1a27f6a2be8f35f61465c
rotem711 <rotemskandrani@gmail.com>
2022-07-17 08:37:21 -0500

    fix: fixing desktop styles

--- components/blocks/quote_slider/quote_slider.tsx
+++ components/blocks/quote_slider/quote_slider.tsx
@@ -1,8 +1,6 @@
-/* eslint-disable react/no-array-index-key */
-/* eslint-disable arrow-body-style */
 import React, { FunctionComponent } from 'react'
 import { Swiper, SwiperSlide } from 'swiper/react'
-import LeftArrow from 'components/generic/icons/leftArrow'
+import { Navigation } from 'swiper'
 import styles from './quote_slider.module.scss'
 import QuoteSliderInterface from './quote_slider.interface'
 
@@ -14,28 +12,29 @@ const QuoteSliderBlock: FunctionComponent<{ block: QuoteSliderInterface }> = ({
   <div className={`${styles.root} container`}>
     <div className={`${styles.headline} typo-h3`}>{block.qs_headline}</div>
     <div className={styles.contentWrapper}>
-      <button type="button">
-        <LeftArrow />
-      </button>
       <Swiper
         spaceBetween={50}
         slidesPerView={1}
         onSlideChange={() => console.log('slide change')}
         onSwiper={(swiper) => console.log(swiper)}
+        navigation
+        modules={[Navigation]}
       >
-        {block.qs_quotes.map((item: any, key: number) => {
-          return (
-            <SwiperSlide key={key}>
-              <div className={styles.quoteWrapper}>
-                <div className={styles.quoteText}>{item.quote}</div>
-                <div className={styles.quoteAuthor}>{item.author}</div>
+        {block.qs_quotes.map((item: any) => (
+          <SwiperSlide>
+            <div className={styles.quoteWrapper}>
+              <div className={`${styles.quoteText} typo-h4`}>{item.quote}</div>
+              <div className={`${styles.quoteAuthor} typo-h6`}>
+                {item.author}
               </div>
-            </SwiperSlide>
-          )
-        })}
+            </div>
+          </SwiperSlide>
+        ))}
       </Swiper>
     </div>
-    <div className={`${styles.headline} typo-h3`}>{block.qs_cta_title}</div>
+    <div className={`${styles.ctaWrapper} typo-p`}>
+      <a href={block.qs_cta_url}>{block.qs_cta_title}</a>
+    </div>
   </div>
 )
 
