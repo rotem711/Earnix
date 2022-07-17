1399ee8c8db3bd6e4e4ec7980073d3919b122a1b
rotem711 <rotemskandrani@gmail.com>
2022-07-17 11:32:46 -0500

    fix: adding resposnive styles

--- components/blocks/quote_slider/quote_slider.interface.ts
+++ components/blocks/quote_slider/quote_slider.interface.ts
@@ -1,7 +1,8 @@
 export default interface QuoteSliderInterface {
-  type: string
-  qs_headline: string
-  qs_cta_title: string
-  qs_cta_url: string
-  qs_quotes: any
+  block: {
+    qs_headline: string
+    qs_cta_title: string
+    qs_cta_url: string
+    qs_quotes: any
+  }
 }
--- components/blocks/quote_slider/quote_slider.tsx
+++ components/blocks/quote_slider/quote_slider.tsx
@@ -1,41 +1,84 @@
-import React, { FunctionComponent } from 'react'
+/* eslint-disable react/destructuring-assignment */
+/* eslint-disable react/no-array-index-key */
+/* eslint-disable arrow-body-style */
+import React from 'react'
 import { Swiper, SwiperSlide } from 'swiper/react'
 import { Navigation } from 'swiper'
+import LeftArrow from 'components/generic/icons/leftArrow'
+import RightArrow from 'components/generic/icons/rightArrow'
 import styles from './quote_slider.module.scss'
 import QuoteSliderInterface from './quote_slider.interface'
 
 export const typename = 'Set_Replicator_BlockQuoteSlider'
 
-const QuoteSliderBlock: FunctionComponent<{ block: QuoteSliderInterface }> = ({
-  block,
-}) => (
-  <div className={`${styles.root} container`}>
-    <div className={`${styles.headline} typo-h3`}>{block.qs_headline}</div>
-    <div className={styles.contentWrapper}>
-      <Swiper
-        spaceBetween={50}
-        slidesPerView={1}
-        onSlideChange={() => console.log('slide change')}
-        onSwiper={(swiper) => console.log(swiper)}
-        navigation
-        modules={[Navigation]}
-      >
-        {block.qs_quotes.map((item: any) => (
-          <SwiperSlide>
-            <div className={styles.quoteWrapper}>
-              <div className={`${styles.quoteText} typo-h4`}>{item.quote}</div>
-              <div className={`${styles.quoteAuthor} typo-h6`}>
-                {item.author}
-              </div>
-            </div>
-          </SwiperSlide>
-        ))}
-      </Swiper>
+const QuoteSliderBlock = (props: QuoteSliderInterface) => {
+  return (
+    <div className={`${styles.root} container`}>
+      <div className={`${styles.headline} typo-h3`}>
+        {props.block.qs_headline}
+      </div>
+      <div className={`${styles.desktopContentWrapper} sm:hidden md:block`}>
+        <Swiper
+          spaceBetween={50}
+          slidesPerView={1}
+          modules={[Navigation]}
+          navigation
+        >
+          {props.block.qs_quotes.map((item: any, key: number) => {
+            return (
+              <SwiperSlide key={key}>
+                <div className={styles.quoteWrapper}>
+                  <div className={`${styles.quoteText} typo-h4`}>
+                    {item.quote}
+                  </div>
+                  <div className={`${styles.quoteAuthor} typo-h6`}>
+                    {item.author}
+                  </div>
+                </div>
+              </SwiperSlide>
+            )
+          })}
+        </Swiper>
+      </div>
+      <div className={`${styles.mobileContentWrapper} sm:block md:hidden`}>
+        <Swiper
+          spaceBetween={50}
+          slidesPerView={1}
+          modules={[Navigation]}
+          navigation={{
+            nextEl: '.review-swiper-button-next',
+            prevEl: '.review-swiper-button-prev',
+          }}
+        >
+          {props.block.qs_quotes.map((item: any, key: number) => {
+            return (
+              <SwiperSlide key={key}>
+                <div className={styles.quoteWrapper}>
+                  <div className={`${styles.quoteText} typo-h4 `}>
+                    {item.quote}
+                  </div>
+                  <div className={`${styles.quoteAuthor} typo-h6`}>
+                    {item.author}
+                  </div>
+                </div>
+              </SwiperSlide>
+            )
+          })}
+        </Swiper>
+      </div>
+      <div className={`${styles.ctaWrapper} typo-p`}>
+        <a href={props.block.qs_cta_url}>{props.block.qs_cta_title}</a>
+      </div>
+      <div className={`${styles.mobileControls} sm:flex sm:flex-row md:hidden`}>
+        <button type="button" className="review-swiper-button-prev">
+          <LeftArrow />
+        </button>
+        <button type="button" className="review-swiper-button-next">
+          <RightArrow />
+        </button>
+      </div>
     </div>
-    <div className={`${styles.ctaWrapper} typo-p`}>
-      <a href={block.qs_cta_url}>{block.qs_cta_title}</a>
-    </div>
-  </div>
-)
+  )
+}
 
 export default QuoteSliderBlock
