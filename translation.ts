3920e1f891415148f470b428a867208d40c2716d
rotem711 <rotemskandrani@gmail.com>
2022-07-18 18:07:17 +0200

    completed generic card component

--- translation.ts
+++ translation.ts
@@ -5,6 +5,9 @@ const Translation = {
   FOLLOW: {
     en_US: 'Follow us on',
   },
+  MONTHS: {
+    en_US: 'January_February_March_April_May_June_July_August_September_October_November_December'.split('_'),
+  },
 }
 
 const translations = (lang): { [key: string]: string } => {
