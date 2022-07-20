f6c3bd2d37d950529e112bd5573776364961729b
rotem711 <rotemskandrani@gmail.com>
2022-07-20 13:55:11 +0200

    Finished blog quote

--- components/blog/quote/quote.tsx
+++ components/blog/quote/quote.tsx
@@ -1,15 +1,16 @@
 import React from 'react'
+import useIsMobile from 'utils/hooks'
 import styles from './quote.module.scss'
 import QuoteInterface from './quote.interface'
 
-const Quote = (data : QuoteInterface) => {
-  const {
-    quote = '',
-  } = data
+const Quote = (data: QuoteInterface) => {
+  const { quote = '' } = data
+
+  const isMobile = useIsMobile('lg')
 
   return (
     <div className={`${styles.root}`}>
-      <h4>{quote}</h4>
+      {isMobile ? <h6>{quote}</h6> : <h4>{quote}</h4>}
     </div>
   )
 }
