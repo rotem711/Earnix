6b639ab5d10b3161515241804d5f3adb84346f97
rotem711 <rotemskandrani@gmail.com>
2022-07-20 13:49:59 +0200

    Blog image component finished

--- components/blog/blog_image/blog_image.tsx
+++ components/blog/blog_image/blog_image.tsx
@@ -1,6 +1,8 @@
-import React from 'react'
+/* eslint-disable jsx-a11y/no-static-element-interactions */
+/* eslint-disable jsx-a11y/click-events-have-key-events */
+import React, { useEffect, useState } from 'react'
 import Image from 'next/image'
-// import Glightbox from 'glightbox'
+import Close from 'components/generic/icons/close'
 
 import styles from './blog_image.module.scss'
 import BlogImageInterface from './blog_image.interface'
@@ -8,34 +10,67 @@ import BlogImageInterface from './blog_image.interface'
 const BlogImage = (data: BlogImageInterface) => {
   const { image, caption } = data
 
-  // useEffect(() => {
-  //   Glightbox({
-  //     selector: 'glightboxTest',
-  //   })
-  // }, [])
+  const [lightboxOpen, setLightboxOpen] = useState(false)
+
+  const toggleLightbox = () => {
+    if (!lightboxOpen) {
+      document.body.style.overflow = 'hidden'
+    } else {
+      document.body.style.overflow = 'auto'
+    }
+
+    setLightboxOpen(!lightboxOpen)
+  }
+
+  useEffect(() => {
+    document.addEventListener('keydown', (event) => {
+      if (event.key === 'Escape') {
+        setLightboxOpen(false)
+      }
+    })
+  }, [])
 
   return (
-    <a
-      href={image.permalink}
-      className={`${styles.root} glightbox`}
-      data-type="image"
-      onClick={async (e) => {
-        e.preventDefault()
-        // Dynamically load GlightBox
-        const GLightbox = (await import('glightbox')).default
-      }}
-    >
-      <Image
-        layout="responsive"
-        width={image.width}
-        height={image.height}
-        src={image.permalink}
-        alt={image.alt}
-        blurDataURL={image.placeholder}
-        placeholder="blur"
-      />
-      <p>{caption}</p>
-    </a>
+    <div className={`${styles.root}`}>
+      <div className={`${styles.imageWrapper}`} onClick={toggleLightbox}>
+        <div>
+          <Image
+            layout="fill"
+            objectFit="cover"
+            objectPosition="center top"
+            width={image.width}
+            height={image.height}
+            src={image.permalink}
+            alt={image.alt}
+            blurDataURL={image.placeholder}
+            placeholder="blur"
+          />
+        </div>
+        <p>{caption}</p>
+      </div>
+      {lightboxOpen && (
+        <div className={`${styles.lightbox}`}>
+          <div
+            className={`${styles.lightboxBackground}`}
+            onClick={toggleLightbox}
+          />
+          <div className={`${styles.lightboxContent}`}>
+            <button type="button" onClick={toggleLightbox}>
+              <Close className="" />
+            </button>
+            <Image
+              layout="intrinsic"
+              width={image.width}
+              height={image.height}
+              src={image.permalink}
+              alt={image.alt}
+              blurDataURL={image.placeholder}
+              placeholder="blur"
+            />
+          </div>
+        </div>
+      )}
+    </div>
   )
 }
 
