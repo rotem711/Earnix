f7189debe21d33cf9bbdf0307c9bf431edc71ea8
rotem711 <rotemskandrani@gmail.com>
2022-07-25 18:27:38 +0300

    fix: Fixed fonts, gaps and grid in CtaBox, ChallengesTeaser and TextIconGrid components

--- components/blocks/cta_box/cta_box.tsx
+++ components/blocks/cta_box/cta_box.tsx
@@ -8,7 +8,7 @@ import Button from '../../generic/button/button'
 export const typename = 'Set_Replicator_BlockCtaBox'
 
 const CtaBoxBlock:FunctionComponent<{ block: CtaBoxInterface }> = ({ block }) => (
-  <div className={`${styles.root} grid grid-cols-12`}>
+  <div className={`${styles.root} container grid grid-cols-12`}>
     <div className={`${styles.contentWrapper} col-start-1 col-span-12 xxl:col-start-3 xxl:col-span-8`}>
       <div className={styles.imageWrapper}>
         <Image
@@ -21,10 +21,10 @@ const CtaBoxBlock:FunctionComponent<{ block: CtaBoxInterface }> = ({ block }) =>
         />
       </div>
       <div className={`${styles.innerContentWrapper}`}>
-        <div className={`typo-h3 ${styles.headline}`}>
+        <div className={`${styles.headline} typo-h3`}>
           {block.cb_headline}
         </div>
-        <div className={`typo-p ${styles.cb_copy}`}>{parse(block.cb_copy)}</div>
+        <div className={`${styles.cb_copy} typo-p`}>{parse(block.cb_copy)}</div>
         <div className={styles.buttonWrapper}>
           <Button href={block.cb_cta_url} title={block.cb_cta_title} />
         </div>
