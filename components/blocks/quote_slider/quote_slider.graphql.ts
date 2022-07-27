56b0ce06443dc8fa928485c360776474a84c3833
rotem711 <rotemskandrani@gmail.com>
2022-07-27 18:21:44 +0200

    QuoteSlider refactored
    fixed arrow assets too

--- components/blocks/quote_slider/quote_slider.graphql.ts
+++ components/blocks/quote_slider/quote_slider.graphql.ts
@@ -1,12 +1,11 @@
 export default `
 fragment BlockQuoteSlider on Set_Replicator_BlockQuoteSlider {
-    type
-    qs_headline
-    qs_cta_title
-    qs_cta_url
-    qs_quotes {
-                  author
-                  quote
-                }
+  qs_headline
+  qs_cta_title
+  qs_cta_url
+  qs_quotes {
+    author
+    quote
+  }
 }
 `
