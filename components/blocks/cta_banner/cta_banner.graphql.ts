e5aaf626e085fb50fb1f7cf76639e5dc19aef8e9
rotem711 <rotemskandrani@gmail.com>
2022-07-28 08:41:46 +0200

    CTA Banner fixes

--- components/blocks/cta_banner/cta_banner.graphql.ts
+++ components/blocks/cta_banner/cta_banner.graphql.ts
@@ -1,14 +1,11 @@
 export default `
 fragment BlockCtaBanner on Set_Replicator_BlockCtaBanner {
-    type
-    ctab_copy
-    ctab_cta_title
-    ctab_cta_url
-    ctab_headline
-    ctab_ctaBackground {
-      ... on Asset_Assets {
-        permalink
-      }
-    }
+  ctab_copy
+  ctab_cta_title
+  ctab_cta_url
+  ctab_headline
+  ctab_ctaBackground {
+    ...CMSAsset
+  }
 }
 `
--- components/blocks/cta_banner/cta_banner.tsx
+++ components/blocks/cta_banner/cta_banner.tsx
@@ -10,26 +10,32 @@ const CtaBannerBlock: FunctionComponent<{ block: CtaBannerInterface }> = ({
   block,
 }) => (
   <div className={`${styles.root}`}>
-    <div className={styles.contentWrapper}>
+    <div className={`${styles.bgImage}`}>
+      <Image
+        src={block.ctab_ctaBackground.permalink}
+        width={block.ctab_ctaBackground.width}
+        height={block.ctab_ctaBackground.height}
+        alt={block.ctab_ctaBackground.alt}
+        layout="fill"
+        objectFit="cover"
+      />
+      <div className={`${styles.imgFilter} blue`} />
+      <div className={`${styles.imgFilter} green`} />
+    </div>
+    <div className={`${styles.contentWrapper} container default-grid`}>
       <div
-        className={`${styles.headline} typo-h3 text-white sm:w-11/12 md:w-2/5 sm:pl-20 sm:pr-20 md:pl-0 md:pr-0`}
+        className={`${styles.headline} col-span-full md:col-span-6 md:col-start-2 lg:col-span-8 lg:col-start-3 xl:col-span-6 xl:col-start-4 xxl:col-span-4 xxl:col-start-5 typo-h3`}
       >
         {block.ctab_headline}
       </div>
       <div
-        className={`${styles.copy} typo-h6 text-white sm:w-11/12 md:w-2/5 sm:pl-20 sm:pr-20 md:pl-0 md:pr-0`}
+        className={`${styles.copy} col-span-full md:col-span-6 md:col-start-2 lg:col-span-6 lg:col-start-4 xl:col-span-4 xl:col-start-5 typo-h6`}
       >
         {block.ctab_copy}
       </div>
-      <div className={styles.ctaWrapper}>
+      <div className="col-span-full">
         <Button href={block.ctab_cta_url} title={block.ctab_cta_title} />
       </div>
-      <Image
-        src={`${block.ctab_ctaBackground.permalink}`}
-        layout="fill"
-        className={styles.bgImage}
-      />
-      <div className={styles.imgFilter} />
     </div>
   </div>
 )
