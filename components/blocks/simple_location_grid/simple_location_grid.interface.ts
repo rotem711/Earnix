5241fc38b48b17a050ea3222323f5bd630a39edb
rotem711 <rotemskandrani@gmail.com>
2022-08-01 06:20:59 -0700

    complete simple location grid (#66)
    * complete simple location grid
    
    * update style with built-in mixins

--- components/blocks/simple_location_grid/simple_location_grid.interface.ts
+++ components/blocks/simple_location_grid/simple_location_grid.interface.ts
@@ -1,8 +1,8 @@
 export default interface SimpleLocationGridInterface {
   type: string
   slg_headline: string
-  slg_locations: {
+  slg_locations: [{
     location_title: string
     phone_number: string
-  }
+  }]
 }
--- components/blocks/simple_location_grid/simple_location_grid.tsx
+++ components/blocks/simple_location_grid/simple_location_grid.tsx
@@ -6,11 +6,30 @@ export const typename = 'Set_Replicator_BlockSimpleLocationGrid'
 
 const SimpleLocationGridBlock: FunctionComponent<{
   block: SimpleLocationGridInterface
-}> = ({ block }) => (
-  <div className={`${styles.root} container`}>
-    Simplelocationgrid Block:
-    {block.type}
-  </div>
-)
+}> = ({ block }) => {
+  const { slg_headline, slg_locations } = block
+  return (
+    <div className={`${styles.root}`}>
+      <div className={`${styles.block_wrapper} container`}>
+        <p className={`${styles.block_title}`}>{slg_headline}</p>
+        <div className="default-grid">
+          {slg_locations.map((location) => (
+            <div
+              className={`col-span-2 sm:col-span-1 md:col-span-2 lg:col-span-3 ${styles.location}`}
+              key={location.location_title}
+            >
+              <p className={`${styles.location_name}`}>
+                {location.location_title}
+              </p>
+              <p className={`${styles.location_phone}`}>
+                {location.phone_number}
+              </p>
+            </div>
+          ))}
+        </div>
+      </div>
+    </div>
+  )
+}
 
 export default SimpleLocationGridBlock
