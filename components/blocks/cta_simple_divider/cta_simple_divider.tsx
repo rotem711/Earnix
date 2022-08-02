bfaa1dfb820cdafddfbc7546c59782722fa28bd1
rotem711 <rotemskandrani@gmail.com>
2022-08-02 14:02:27 +0300

    feat: Added CtaSimpleDivider component (#71)
    Co-authored-by: valentinschreibmaier <valentin.schreibmaier+metajive@pxlp.dev>

--- components/blocks/cta_simple_divider/cta_simple_divider.tsx
+++ components/blocks/cta_simple_divider/cta_simple_divider.tsx
@@ -1,16 +1,53 @@
 import React, { FunctionComponent } from 'react'
+import cn from 'classnames'
 import styles from './cta_simple_divider.module.scss'
 import CtaSimpleDividerInterface from './cta_simple_divider.interface'
+import LogoIcon from './logo_icon/logo_icon'
+import Button from '../../generic/button/button'
 
 export const typename = 'Set_Replicator_BlockCtaSimpleDivider'
 
 const CtaSimpleDividerBlock: FunctionComponent<{
   block: CtaSimpleDividerInterface
-}> = ({ block }) => (
-  <div className={`${styles.root} container`}>
-    Ctasimpledivider Block:
-    {block.type}
-  </div>
-)
+}> = ({ block }) => {
+  const isWithButton = block.csd_cta_url !== null && block.csd_link_text !== null
+
+  return (
+    <div className={`${styles.root} container grid grid-cols-12 default-grid`}>
+      <div
+        className={cn(
+          styles.contentWrapper,
+          {
+            [`${styles.contentWrapperWithButton}`]: isWithButton,
+          },
+          'col-start-1',
+          'col-span-12',
+        )}
+      >
+        {!isWithButton && (
+          <div className={`${styles.logoWrapper}`}>
+            <LogoIcon />
+          </div>
+        )}
+        <div
+          className={cn(
+            styles.headline,
+            {
+              [`${styles.headlineWithButton}`]: isWithButton,
+            },
+            'typo-h4',
+          )}
+        >
+          {block.csd_headline}
+        </div>
+        {isWithButton && (
+          <div className={`${styles.buttonWrapper}`}>
+            <Button href={block.csd_cta_url} title={block.csd_link_text} />
+          </div>
+        )}
+      </div>
+    </div>
+  )
+}
 
 export default CtaSimpleDividerBlock
---
+++ interfaces/svg.ts
@@ -0,0 +1,6 @@
+export default interface SvgIconInterface {
+    color?: string;
+    width?: string;
+    height?: string;
+    customClass?: string;
+}
