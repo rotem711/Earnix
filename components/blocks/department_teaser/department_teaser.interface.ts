d854d472ec80144aec377608f08206f1ff192a4d
rotem711 <rotemskandrani@gmail.com>
2022-08-03 18:46:15 +0300

    feat: Added DepartmentTeaser component (#78)

--- components/blocks/department_teaser/department_teaser.interface.ts
+++ components/blocks/department_teaser/department_teaser.interface.ts
@@ -1,8 +1,8 @@
 export default interface DepartmentTeaserInterface {
   type: string
   dt_headline: string
-  dt_departments: {
+  dt_departments: Array<{
     comeet_department_id: string
     department_label: string
-  }
+  }>
 }
--- components/blocks/department_teaser/department_teaser.tsx
+++ components/blocks/department_teaser/department_teaser.tsx
@@ -7,9 +7,18 @@ export const typename = 'Set_Replicator_BlockDepartmentTeaser'
 const DepartmentTeaserBlock: FunctionComponent<{
   block: DepartmentTeaserInterface
 }> = ({ block }) => (
-  <div className={`${styles.root} container`}>
-    Departmentteaser Block:
-    {block.type}
+  <div className={`${styles.root} container grid grid-cols-12 default-grid`}>
+    <div className={`${styles.contentWrapper} col-start-1 col-span-12`}>
+      <div className={`${styles.headline} typo-h3`}>{block.dt_headline}</div>
+      <div className={styles.departmentsWrapper}>
+        {block.dt_departments.map((department) => (
+          <div className={styles.department} key={department.department_label}>
+            <div className={`${styles.departmentLabel} typo-h6`}>{department.department_label}</div>
+            <div className={`${styles.jobsInLocations} typo-p`}>x jobs in y locations</div>
+          </div>
+        ))}
+      </div>
+    </div>
   </div>
 )
 
