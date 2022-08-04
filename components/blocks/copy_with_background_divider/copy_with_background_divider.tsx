e19538642385c957a042cfdb514e3861a7f384a3
rotem711 <rotemskandrani@gmail.com>
2022-08-04 14:32:02 +0300

    feat: Added CopyWithBackgroundDivider component (#81)

--- components/blocks/copy_with_background_divider/copy_with_background_divider.tsx
+++ components/blocks/copy_with_background_divider/copy_with_background_divider.tsx
@@ -1,4 +1,6 @@
 import React, { FunctionComponent } from 'react'
+import parse from 'html-react-parser'
+import Image from 'next/image'
 import styles from './copy_with_background_divider.module.scss'
 import CopyWithBackgroundDividerInterface from './copy_with_background_divider.interface'
 
@@ -7,9 +9,25 @@ export const typename = 'Set_Replicator_BlockCopyWithBackgroundDivider'
 const CopyWithBackgroundDividerBlock: FunctionComponent<{
   block: CopyWithBackgroundDividerInterface
 }> = ({ block }) => (
-  <div className={`${styles.root} container`}>
-    Copywithbackgrounddivider Block:
-    {block.type}
+  <div className={`${styles.root}`}>
+    <div className={styles.imageWrapper}>
+      <Image
+        layout="fill"
+        objectFit="cover"
+        src={block.cwb_background_image.permalink}
+        alt={block.cwb_background_image.alt}
+        placeholder="blur"
+        blurDataURL={block.cwb_background_image.placeholder}
+      />
+      <div className={styles.imageFilterBlack} />
+      <div className={styles.imageFilterGreen} />
+    </div>
+    <div className="container grid grid-cols-12 default-grid">
+      <div className={`${styles.contentWrapper} col-start-1 col-span-12`}>
+        <div className={`${styles.headline} typo-h3`}>{block.cwb_headline}</div>
+        <div className={`${styles.copy} typo-h6`}>{parse(block.cwb_copy)}</div>
+      </div>
+    </div>
   </div>
 )
 
